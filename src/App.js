5d68ffb545db3d2022bb7f38d2211f6dd4c25550
Alyssa Wiser <alyssa.wiser@mykenzie.snhu.edu>
2023-02-27 09:47:57 -0500

    removed browserRouter in App.js 02/27/23

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
